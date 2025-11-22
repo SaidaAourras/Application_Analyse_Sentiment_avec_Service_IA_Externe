4933a4d158a54f8cb23599f260733054075f579c
SaidaAourras <sa123aouras@gmail.com>
2025-11-22 12:29:00 +0100

    delete a useless file

--- test_hugging_face.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-model = pipeline("sentiment-analysis" , model="nlptown/bert-base-multilingual-uncased-sentiment")
-
-response = model("text to analyse")
-
-print(response)
